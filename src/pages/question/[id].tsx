a1e0d792547ec31642e7085005e9894fdb2a66f2
Mathias Chevez <solo@Mathiass-MacBook-Air.local>
2022-06-25 00:32:27 -0600

    db model and options

--- src/pages/question/[id].tsx
+++ src/pages/question/[id].tsx
@@ -6,7 +6,18 @@ const QuestionsPageContent: React.FC<{ id: string }> = ({ id }) => {
 
   if(!isLoading && !data) return <div>Question not found...</div>
 
-  return <div>{data?.question}</div>
+  return( 
+    <div className='flex flex-col p-6'>
+      <h1 className='text-2xl font-bold'>{data?.question}</h1>
+      <div>
+        {(data?.options as string[])?.map(option => 
+          <div key={option}>
+            <h1>Option {option}</h1>
+          </div>
+        )}
+      </div>
+    </div>
+  ) 
 }
 
 const QuestionPage = () => {
