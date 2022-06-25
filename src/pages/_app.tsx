881dc8dbaa545ff089e976b189d6b2595a6148f3
Mathias Chevez <solo@Mathiass-MacBook-Air.local>
2022-06-24 23:42:16 -0600

    fixing cors issues

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,9 +15,7 @@ export default withTRPC<AppRouter>({
      * If you want to use SSR, you need to use the server's full URL
      * @link https://trpc.io/docs/ssr
      */
-    const url = process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/api/trpc`
-      : 'http://localhost:3000/api/trpc';
+    const url = '/api/trpc';
 
     return {
       url,
@@ -32,5 +30,5 @@ export default withTRPC<AppRouter>({
   /**
    * @link https://trpc.io/docs/ssr
    */
-  ssr: true,
+  ssr: false,
 })(MyApp);
